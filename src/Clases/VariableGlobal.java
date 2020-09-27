00a4eca7e2d0c2c68988d7fe376f3bc7c0d0da25
Daniel <josetisrey@gmail.com>
2020-09-27 10:51:27 -0600

    Hice global la clase TablaID

--- src/Clases/VariableGlobal.java
+++ src/Clases/VariableGlobal.java
@@ -23,16 +23,18 @@ public class VariableGlobal {
         contra = "hacker.net";
         fichero = new Fichero();
         */
-        /*host = "localhost";
+        
+        host = "localhost";
         puerto = "3305";
         usuario = "root";
         contra = "xela2020";
-        */
+        
+        /*
         host = "localhost";
         puerto = "3306";
         usuario = "root";
         contra = "master502";
-        
+        */
         
         fichero = new Fichero();
         this.conexionMySQL = new ConexionMySQL(host, puerto, fichero.obterBase(), usuario, contra);
@@ -45,17 +47,19 @@ public class VariableGlobal {
         usuario = "root";
         contra = "hacker.net";
         fichero = new Fichero();
-*/
-        /*host = "localhost";
-        puerto = "3306";
+
+        */
+        host = "localhost";
+        puerto = "3305";
         usuario = "root";
         contra = "xela2020";
-        */
+        
+        /*
         host = "localhost";
         puerto = "3306";
         usuario = "root";
         contra = "master502";
-        
+        */
         fichero = new Fichero();
         
         this.conexionMySQL = new ConexionMySQL(host, puerto, base, usuario, contra);
